6f1271c075ade4694f42d9ec408c4283f7e7fd04
Nishad <107932663+Nsd654@users.noreply.github.com>
2026-01-06 00:35:30 +0530

    Remove petal animation

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,6 @@ import { useEffect, useState, useRef } from "react";
 
 
 export default function Home() {
-//   const touchStartX = useRef(0);
-// const touchEndX = useRef(0);
 
   const slides = [
     <div key="1">
@@ -46,41 +44,12 @@ export default function Home() {
     return () => clearInterval(timer);
   }, []);
 
-  // useEffect(() => {
-  //   for (let i = 0; i < 25; i++) {
-  //     const petal = document.createElement("div");
-  //     petal.className = "petal";
-  //     petal.style.left = Math.random() * 100 + "vw";
-  //     petal.style.animationDuration = 6 + Math.random() * 6 + "s";
-  //     document.body.appendChild(petal);
-  //   }
-  // }, []);
-//   const handleTouchStart = (e: React.TouchEvent<HTMLDivElement>) => {
-//   touchStartX.current = e.touches[0].clientX;
-// };
-
-// const handleTouchMove = (e: React.TouchEvent<HTMLDivElement>) => {
-//   touchEndX.current = e.touches[0].clientX;
-// };
-
-// const handleTouchEnd = () => {
-//   if (touchStartX.current - touchEndX.current > 50) {
-//     setIndex((prev) => (prev + 1) % slides.length);
-//   }
-
-//   if (touchEndX.current - touchStartX.current > 50) {
-//     setIndex((prev) => (prev - 1 + slides.length) % slides.length);
-//   }
-// };
 
 
 
   return (
   <main
   className="container"
-  // onTouchStart={handleTouchStart}
-  // onTouchMove={handleTouchMove}
-  // onTouchEnd={handleTouchEnd}
 >
     <div key={index} className="slide fade">
       {slides[index]}
